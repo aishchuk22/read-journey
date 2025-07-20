68bcf8ccc9c57f3b6569dea71c82e02f949c3a4d
Andrii_22 <a_ishchuk22@ukr.net>
2025-07-20 21:46:22 +0300

    added burger initial logic

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,46 @@
-import styles from "./Header.module.css";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Menu } from "lucide-react";
-import logo from "../../assets/react.svg";
 import { logout } from "../../redux/auth/authOperations";
+import { Menu, X } from "lucide-react";
+import logo from "../../assets/react.svg";
+import styles from "./Header.module.css";
 
 const Header = () => {
   const dispatch = useDispatch();
   const name = useSelector((state) => state.auth.user?.name || "");
   const firstLetter = name.charAt(0).toUpperCase();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleLogout = () => {
     dispatch(logout());
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
     <header className={styles.header}>
       <img src={logo} alt="Logo" className={styles.logo} />
+
       <div className={styles.right}>
         <div className={styles.avatar}>{firstLetter}</div>
+
         <button onClick={handleLogout} className={styles.logout}>
           Logout
         </button>
-        <Menu className={styles.burger} size={28} />
+
+        <button
+          onClick={toggleMenu}
+          className={styles.burgerBtn}
+          aria-label="Toggle menu"
+        >
+          {isMenuOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
       </div>
+
+      {/* компонент MobileMenu */}
+      {/* {isMenuOpen && <MobileMenu onClose={toggleMenu} />} */}
     </header>
   );
 };
